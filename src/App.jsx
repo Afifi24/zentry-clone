249f0a2247f69cfdb73c59c356b9f87eac0a4ee9
afifi brahim <afifi71brahim@gmail.com>
2024-11-24 00:07:43 +0100

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-violet-300 text-5xl font-bold ">
+      welcom to the whole world{" "}
+    </div>
+  );
+};
+
+export default App;
