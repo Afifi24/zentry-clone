7e39601743267d8df4e263be8699586aae77d2d2
afifi brahim <afifi71brahim@gmail.com>
2024-11-25 15:51:54 +0100

    add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
     <main className="relative w-screen overflow-x-hidden min-h-screen ">
       <Hero />
-      <section className="min-h-screen bg-blue-600"></section>
+      <About />
     </main>
   );
 };
